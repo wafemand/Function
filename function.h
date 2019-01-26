f364d8a85191ae219da8c33d538bafab2eeeae05
Zaharov Andrey <andrey150399@mail.ru>
2019-01-26 17:57:07 +0300

    upd

--- function.h
+++ function.h
@@ -11,9 +11,11 @@ class Function;
 template<typename R, typename ...Args>
 struct Function<R(Args...)> {
 
-    static const int BUF_SIZE = 42;
+    static const int BUF_SIZE = 16;
 
-    Function(std::nullptr_t) : concept() {
+    Function() noexcept : Function(nullptr) {}
+
+    Function(std::nullptr_t) noexcept : concept() {
         isSmall = false;
     }
 
@@ -26,7 +28,7 @@ struct Function<R(Args...)> {
         }
     }
 
-    Function(Function &&other) {
+    Function(Function &&other) noexcept {
         if (other.isSmall) {
             other.get_concept()->placement_move(buf);
         } else {
@@ -45,30 +47,8 @@ struct Function<R(Args...)> {
         }
     }
 
-    void destroy() {
-        if (isSmall) {
-            get_concept()->~Concept();
-        } else {
-            concept.~unique_ptr();
-        }
-    }
-
     ~Function() {
         destroy();
-    }
-
-    void swap(Function<R(Args...)> &other) {
-        Function temp(std::move(*this));
-        *this = std::move(other);
-        other = std::move(temp);
-    }
-
-    R operator()(Args ...args) {
-        return get_concept()->invoke(args...);
-    }
-
-    R operator()(Args ...args) const {
-        return get_concept()->invoke(args...);
     }
 
     Function &operator=(Function other) {
@@ -79,26 +59,44 @@ struct Function<R(Args...)> {
     Function &operator=(Function &&other) noexcept {
         destroy();
         swap(other);
+        return *this;
+    }
+
+    void swap(Function &other) noexcept {
+        Function temp(std::move(*this));
+        new (this) Function(std::move(other));
+        new (&other) Function(std::move(temp));
+    }
+
+    operator bool() {
+        return isSmall && concept;
+    }
+
+    R operator()(Args ...args) const {
+        return get_concept()->invoke(args...);
+    }
+
+    void destroy() {
+        if (isSmall) {
+            get_concept()->~Concept();
+        } else {
+            concept.~unique_ptr();
+        }
     }
 
     struct Concept {
-        virtual R invoke(Args... args) = 0;
         virtual R invoke(Args... args) const = 0;
         virtual void placement_copy_small(void *ptr) const = 0;
         virtual void placement_copy_big(void *ptr) const = 0;
-        virtual void placement_move(void *ptr) = 0;
+        virtual void placement_move(void *ptr) noexcept = 0;
     };
 
     template<typename F>
     struct Model : Concept {
         explicit Model(F f) : f(std::move(f)) {}
 
-        R invoke(Args... args) {
-            return f(std::forward(args)...);
-        }
-
         R invoke(Args... args) const {
-            return f(std::forward(args)...);
+            return f(std::forward<Args>(args)...);
         }
 
         void placement_copy_small(void *ptr) const {
@@ -109,12 +107,11 @@ struct Function<R(Args...)> {
             new(ptr) std::unique_ptr<Concept>(new Model<F>(f));
         }
 
-        void placement_move(void *ptr) {
+        void placement_move(void *ptr) noexcept {
             new(ptr) Model<F>(std::move(f));
         }
 
-        mutable F f;
-        // because f::operator() can be not const;
+        F f;
     };
 
     const Concept *get_concept() const {
--- main.cpp
+++ main.cpp
@@ -1,40 +1,107 @@
 
 #include <iostream>
+#include <cassert>
 #include "function.h"
 
 using namespace std;
 
-int kek(int, int) {
-    return 100;
-}
+int copies = 0;
+int moves = 0;
+
+struct BigData {
+    long long a[42];
+};
 
 
-struct QQQ{
-    QQQ(const QQQ&){
-        cout << "copy QQQ\n";
+template<typename ForSize>
+struct CopyMoveCounter {
+    ForSize forSize;
+
+    CopyMoveCounter(const CopyMoveCounter &) {
+        copies++;
     }
-    QQQ(QQQ&&){
-        cout << "move QQQ\n";
+
+    CopyMoveCounter(CopyMoveCounter &&) {
+        moves++;
     }
-    QQQ() = default;
-    int operator()(){
-        return 228;
+
+    CopyMoveCounter() {
+        copies = moves = 0;
+    };
+
+    int operator()() const {
+        return 42;
     }
 };
 
 
-int main() {
-    Function<int(int, int)> lol(kek);
+int foo(int a, int b) {
+    return a + b;
+}
 
-    uint32_t q = 0xFFFFFFFF;
-    Function<int(int, int)> mem([q](int a, int b) {
+void test_function_pointer() {
+    Function<int(int, int)> f = foo;
+    assert(foo(50, 60) == 110);
+}
+
+
+void test_lambda() {
+    auto bar = [](int a, int b) {
         return a + b;
-    });
+    };
+    Function<int(int, int)> f = bar;
+    assert(foo(50, 60) == 110);
+}
 
-    Function<int()> qqq = QQQ();
-    //const std::function<int()> qqq = QQQ();
 
-    auto qqq2 = std::move(qqq);
-    cout << lol(10, 20) << ' ' << mem(20, 30) << ' ' << qqq2();
+void test_copy_small() {
+    CopyMoveCounter<int> cmc;
+    Function<int()> f = cmc;
+    assert(copies == 1);
+}
 
+
+void test_move_small() {
+    CopyMoveCounter<int> cmc;
+    Function<int()> f = std::move(cmc);
+    assert(copies == 0);
+}
+
+
+void test_copy_big() {
+    CopyMoveCounter<BigData> cmc;
+    Function<int()> f = cmc;
+    assert(copies == 1);
+}
+
+
+void test_move_big() {
+    CopyMoveCounter<BigData> cmc;
+    Function<int()> f = std::move(cmc);
+    assert(copies == 0);
+}
+
+
+void test_swap() {
+    CopyMoveCounter<int> a;
+    auto b = []() { return 10; };
+    Function<int()> f = a;
+    Function<int()> g = b;
+
+    assert(f() == 42);
+    assert(g() == 10);
+
+    f.swap(g);
+
+    assert(f() == 10);
+    assert(g() == 42);
+}
+
+int main() {
+    test_function_pointer();
+    test_lambda();
+    test_copy_big();
+    test_copy_small();
+    test_move_big();
+    test_move_small();
 }
